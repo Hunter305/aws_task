25f6631bca46ab030705d68d530fe18007f6de67
Hunter305 <avinash8169@gmail.com>
2024-03-08 18:33:14 +0530

    completed media connect

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { startChannel, stopChannel } from "./services.js";
+import { startChannel, startMediaConnect, stopChannel, stopMediaConnect } from "./services.js";
 dotenv.config();
 
 const port = 3000;
@@ -12,6 +12,9 @@ app.get("/", (req, res) => {
 
 app.get("/startchannel", startChannel);
 app.get("/stopChannel", stopChannel);
+
+app.get("/startmediaconnection", startMediaConnect);
+app.get("/stopmediaconnection", stopMediaConnect);
 
 app.listen(port, () => {
   console.log(`app is running in port ${port}`);
--- services.js
+++ services.js
@@ -1,6 +1,11 @@
 import { MediaLiveClient, StartChannelCommand, StopChannelCommand } from "@aws-sdk/client-medialive";
+import { MediaConnect, StartFlowCommand, StopFlowCommand } from "@aws-sdk/client-mediaconnect";
 
 const client = new MediaLiveClient({ region: "ap-south-1" });
+const mediaConnClient = new MediaConnect({ region: "ap-south-1" });
+const mediaConnArn = process.env.MEDIACONNARN;
+
+//media live
 
 const startChannel = async (req, res) => {
   try {
@@ -8,7 +13,7 @@ const startChannel = async (req, res) => {
     const response = await client.send(command);
     res.status(200).json({ message: "Channel started successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Error starting channel", error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -22,4 +27,26 @@ const stopChannel = async (req, res) => {
   }
 };
 
-export { startChannel, stopChannel };
+//media connect
+
+const startMediaConnect = async (req, res) => {
+  try {
+    const command = new StartFlowCommand({ FlowArn: "arn:aws:mediaconnect:ap-south-1:891377081681:flow:1-Xl5QUQtUVVNWBF0G-935895f8ddb4:testamagi" });
+    const response = await mediaConnClient.send(command);
+    res.status(200).json({ message: "flow started successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const stopMediaConnect = async (req, res) => {
+  try {
+    const command = new StopFlowCommand({ FlowArn: "arn:aws:mediaconnect:ap-south-1:891377081681:flow:1-Xl5QUQtUVVNWBF0G-935895f8ddb4:testamagi" });
+    const response = await mediaConnClient.send(command);
+    res.status(200).json({ message: "flow started successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export { startChannel, stopChannel, startMediaConnect, stopMediaConnect };
